4ac05f952675d1509062e1928ba7ebaa6618e8f6
Boss <abhijitmalikrj@gmail.com>
2024-02-13 11:42:33 +0530

    button add

--- app/src/main/java/com/example/fcmtest/MainActivity.java
+++ app/src/main/java/com/example/fcmtest/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.fcmtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button batton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        batton.findViewById(R.id.cat);
+
+
+        batton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(this,"wwq",Toast.LENGTH_LONG0).show();
+                Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
